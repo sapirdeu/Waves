de3ca1607c6490c3e78a137f190f1ca47e7fc5ae
sapirdeu <sapiroosh.d@gmail.com>
2020-09-29 14:41:44 +0300

    finished server

--- server/server.js
+++ server/server.js
@@ -17,10 +17,70 @@ app.use(cookieParser());
 const { User } = require('./models/user');
 const { Brand } = require('./models/brand');
 const { Wood } = require('./models/wood');
+const { Product } = require('./models/product');
 
 // Middlewares
 const {auth} = require('./middleware/auth')
 const {admin} = require('./middleware/admin')
+
+//=================================
+//             PRODUCTS
+//=================================
+
+// Getting product by order
+// sort by ARRIVAL: /api/product/articles?sortBy=createdAt&order=desc&limit=4
+// sort by SELL: /api/product/articles?sortBy=sold&order=desc&limit=4
+app.get('/api/product/articles', (req, res)=>{
+    let order = req.query.order ? req.query.order :  'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+    Product
+    .find()
+    .populate('brand')
+    .populate('wood')
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, articles)=>{
+        if(err) return res.status(400).send(err);
+        return res.status(200).send(articles);
+    })
+});
+
+// Getting products by ID
+// /api/product/articles_by_id?id=123&type=single
+// /api/product/articles_by_id?id=123,456&type=array
+app.get('/api/product/articles_by_id', (req, res)=>{
+    let type = req.query.type;
+    let items = req.query.id;
+
+    if(type === "array"){
+        let ids = req.query.id.split(',');
+        items = [];
+        items = ids.map(item => {
+            return mongoose.Types.ObjectId(item)
+        })
+    }
+
+    Product
+    .find({'_id':{$in:items}})
+    .populate('brand')
+    .populate('wood')
+    .exec((err, docs)=>{
+        if(err) return res.status(400).send(err);
+        return res.status(200).send(docs)
+    })
+});
+
+app.post('/api/product/article', auth, admin, (req, res)=>{
+    const product = new Product(req.body);
+    product.save((err,doc)=>{
+        if(err){
+            return res.json({success: false, err})
+        }
+        res.status(200).json({success: true, article: doc})       
+    })   
+});
 
 //=================================
 //             WOODS
