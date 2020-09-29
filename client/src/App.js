428cddfc2a0b0f1fbb4c19a2eccde80efdf97c63
sapirdeu <sapiroosh.d@gmail.com>
2020-09-29 16:27:15 +0300

    proxy + concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
+
 
 function App() {
+  useEffect(() => {
+    axios.get('/api/product/brands').then(response=>{
+      console.log(response)
+    })
+  }, []);
+
   return (
     <div className="App">
       my app
